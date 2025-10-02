81e9753b3508da25ca06cd079fee27395fddcb8a
Flavio Ricardo <fhricardo@gmail.com>
2025-10-02 17:33:49 -0300

    Update 2025-10-02-1733: Shutdown Screen - Log Off and Cancel Buttons

--- js/shutdown.js
+++ js/shutdown.js
@@ -1,17 +1,25 @@
-const shutdown = document.getElementById('btnShutdown');
-const modal = document.getElementById('logOutScreen');
-const audio = new Audio("assets/audio/win-xp-shutdown.mp3");
+const shutdown = document.getElementById('btnShutdown')
+const logoff = document.getElementById('logoff')
+const cancel = document.getElementById('cancel')
+const modal = document.getElementById('logOutScreen')
+const audio = new Audio("assets/audio/win-xp-shutdown.mp3")
 
 shutdown.addEventListener('click', () => {
     // Abre o modal
-    modal.style.display = 'flex';
+    modal.style.display = 'flex'
 
     // Reproduz o som
-    audio.currentTime = 0;
-    audio.play();
+    audio.currentTime = 0
+    audio.play()
 
     // Redireciona após 6 segundos
     setTimeout(() => {
         window.location.href = "shutdown.html"; // altere para o destino desejado
-    }, 6000);
-});
+    }, 6000)
+})
+cancel.addEventListener('click', () => {
+    window.location.href = 'home.html'
+})
+logoff.addEventListener('click', () => {
+    window.location.href = 'index.html'
+})
